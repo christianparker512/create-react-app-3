4e3272ddc887d2b1e951a99ddd7d5c35faaf391e
Christian Parker <christianparker512@gmail.com>
2021-04-19 12:36:38 -0500

    Adding Javascript, specifically consts into react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,11 +3,15 @@ import './ExpenseItem.css';
 
 function ExpenseItem(){
 
+    const expenseDate = new Date(2021,3, 19);
+    const expenseTitle = 'D & D Detailing';
+    const expenseAmount = 294.37;
+
     return <div className="expense-item">
-                <div>April 19 2021</div>
+                <div>{expenseDate.toISOString()}</div>
                 <div className="expense-item__description">
-                    <h2>Detail</h2>
-                    <div className="expense-item__price">$37.37</div>
+                    <h2>{expenseTitle}</h2>
+                    <div className="expense-item__price">${expenseAmount}</div>
                 </div>
             </div>;
 }
