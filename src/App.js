4a5b3131162a011f308b1ef9e9b1a6de5278b476
Christian Parker <christianparker512@gmail.com>
2021-04-19 12:26:16 -0500

    stying with css and react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
   return (
     <div>
       <h2>D & D Car Wash</h2>
         <p>A New Level of Clean</p>
+        <ExpenseItem></ExpenseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+//Component code written in react is just a function.
+import './ExpenseItem.css';
+
+function ExpenseItem(){
+
+    return <div className="">
+                <div>April 19 2021</div>
+                <div className="expense-item_description">
+                    <h2>Detail</h2>
+                    <div className="expense-item__price">$37.37</div>
+                </div>
+            </div>;
+}
+export default ExpenseItem;
