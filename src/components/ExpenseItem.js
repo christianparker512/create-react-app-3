efe63953c8823d5a39d6caf8984e23be0751508f
Christian Parker <christianparker512@gmail.com>
2021-04-19 12:28:46 -0500

    Final inital css

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,9 +3,9 @@ import './ExpenseItem.css';
 
 function ExpenseItem(){
 
-    return <div className="">
+    return <div className="expense-item">
                 <div>April 19 2021</div>
-                <div className="expense-item_description">
+                <div className="expense-item__description">
                     <h2>Detail</h2>
                     <div className="expense-item__price">$37.37</div>
                 </div>
