af0504cda48f9fe90d82b81144f9fa421437d899
Christian Parker <christianparker512@gmail.com>
2021-04-20 11:00:51 -0500

    react expenses and card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+
+    const expenses = [
+        {
+            id: 'e1',
+            title: 'rags',
+            amount: 97.32,
+            date: new Date (2021, 4, 18),
+        },
+        {
+            id: 'e2',
+            title: 'polish',
+            amount: 197.32,
+            date: new Date (2021, 4, 21),
+        },
+        {
+            id: 'e3',
+            title: 'soap',
+            amount: 57.32,
+            date: new Date (2021, 4, 19),
+        },
+        {
+            id: 'e4',
+            title: 'windex',
+            amount: 27.32,
+            date: new Date (2021, 4, 4),
+        },
+    ]
   return (
     <div>
       <h2>D & D Car Wash</h2>
         <p>A New Level of Clean</p>
-        <ExpenseItem></ExpenseItem>
+        <ExpenseItem title={expenses[0].title}
+                     amount={expenses[0].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+            title={expenses[1].title}
+            amount={expenses[1].amount}
+        ></ExpenseItem>
+        <ExpenseItem
+            title={expenses[2].title}
+            amount={expenses[2].amount}
+        ></ExpenseItem>
+       <ExpenseItem
+           title={expenses[3].title}
+           amount={expenses[3].amount}
+       ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,17 @@
-//Component code written in react is just a function.
+import ExpenseDate from './ExpenseDate';
+import Card from './Card';
 import './ExpenseItem.css';
 
-function ExpenseItem(){
-
-    const expenseDate = new Date(2021,3, 19);
-    const expenseTitle = 'D & D Detailing';
-    const expenseAmount = 294.37;
-
-    return <div className="expense-item">
-                <div>{expenseDate.toISOString()}</div>
-                <div className="expense-item__description">
-                    <h2>{expenseTitle}</h2>
-                    <div className="expense-item__price">${expenseAmount}</div>
-                </div>
-            </div>;
+function ExpenseItem(props) {
+    return (
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date} />
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    );
 }
+
 export default ExpenseItem;
